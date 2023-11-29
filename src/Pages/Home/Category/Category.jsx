77bb019bd11e519aff1bdf1cf492bea0fe7e8d00
rohan future devoloper <xrohaislam373@gmail.com>
2023-11-29 15:08:04 +0600

    category

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -1,8 +1,8 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/free-mode";
 import "swiper/css/pagination";
-import { FreeMode, Pagination } from "swiper/modules";
+import { Pagination } from "swiper/modules";
+
 import slide1 from "../../../assets/home/slide1.jpg";
 import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
@@ -11,39 +11,24 @@ import slide5 from "../../../assets/home/slide5.jpg";
 
 const Category = () => {
   return (
-    <div className="bg-white py-20">
-      <Swiper
-        slidesPerView={4}
-        spaceBetween={30}
-        freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
+    <Swiper
+      slidesPerView={4}
+      spaceBetween={50}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <div className="relative w-full">
           <img src={slide1} alt="" />
-          <h3 className="uppercase text-center -mt-10 text-white text-2xl">salads</h3>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide2} alt="" />
-          <h3 className="uppercase text-center -mt-10 text-white text-2xl">soups</h3>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide3} alt="" />
-          <h3 className="uppercase text-center -mt-10 text-white text-2xl">pizzas</h3>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide4} alt="" />
-          <h3 className="uppercase text-center -mt-10 text-white text-2xl">desserts</h3>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide5} alt="" />
-          <h3 className="uppercase text-center -mt-5 text-white text-2xl">burgers</h3>
-        </SwiperSlide>
-      </Swiper>
-    </div>
+          <h3 className="text-white bottom-10 left-16 absolute  text-4xl  uppercase">
+            salads{" "}
+          </h3>
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 };
 
