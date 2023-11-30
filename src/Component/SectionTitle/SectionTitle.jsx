fb6ce9e1d30a044c6756dcf4b75bb8bfef82579d
rohan future devoloper <xrohaislam373@gmail.com>
2023-11-30 15:00:53 +0600

    updated

--- src/Component/SectionTitle/SectionTitle.jsx
+++ src/Component/SectionTitle/SectionTitle.jsx
@@ -2,7 +2,7 @@ const SectionTitle = ({ subHeading, heading }) => {
   return (
     <div className="mx-auto  md:w-4/12 text-center my-16">
       <p className="text-yellow-600 text-2xl mb-2">----{subHeading}----</p>
-      <h3 className="text-4xl uppercase border-y-4 py-4  text-white">
+      <h3 className="text-4xl uppercase border-y-4 py-4 text-white">
         {heading}
       </h3>
     </div>
---
+++ src/Pages/Home/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../../../../Component/SectionTitle/SectionTitle";
+
+const PopularMenu = () => {
+  return (
+    <section>
+      <SectionTitle
+        subHeading="Check it out"
+        heading="FROM OUR MENU"
+      ></SectionTitle>
+    </section>
+  );
+};
+
+export default PopularMenu;
