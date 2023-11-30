397761c74698f8133d4bd67b475c1a233b79a16e
rohan future devoloper <xrohaislam373@gmail.com>
2023-11-30 15:16:38 +0600

    popular Item

--- src/Pages/Home/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/Home/PopularMenu/PopularMenu.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import SectionTitle from "../../../../Component/SectionTitle/SectionTitle";
+import { useState } from "react";
 
 const PopularMenu = () => {
+  const { menu, setMenu } = useState([]);
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const popularItems = data.filter((item) => item.category === "popular");
+        setMenu(popularItems);
+      });
+  }, []);
   return (
     <section>
       <SectionTitle
