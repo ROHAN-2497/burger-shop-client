d19465766d581c3b9d69ddd626463ab51f086c9c
rohan future devoloper <xrohaislam373@gmail.com>
2023-11-30 14:30:38 +0600

    updated

--- src/Component/SectionTitle/SectionTitle.jsx
+++ src/Component/SectionTitle/SectionTitle.jsx
@@ -1,11 +1,10 @@
-
-const SectionTitle = ({subHeading, heading}) => {
-    return (
-        <div>
-            <p>{subHeading}</p>
-            <h3>{heading}</h3>
-        </div>
-    );
+const SectionTitle = ({ subHeading, heading }) => {
+  return (
+    <div>
+      <p className="text-yellow-500 ">----{subHeading}----</p>
+      <h3 className="text-4xl uppercase border-y-2">{heading}</h3>
+    </div>
+  );
 };
 
 export default SectionTitle;
