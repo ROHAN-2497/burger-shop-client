52f090350f4926351d3fef73ba050874e0387507
rohan future devoloper <xrohaislam373@gmail.com>
2023-12-02 00:28:40 +0600

    updated

--- src/Component/SectionTitle/SectionTitle.jsx
+++ src/Component/SectionTitle/SectionTitle.jsx
@@ -1,7 +1,7 @@
 const SectionTitle = ({ subHeading, heading }) => {
   return (
     <div className="mx-auto  md:w-4/12 text-center my-16">
-      <p className="text-yellow-600 text-2xl mb-2">----{subHeading}----</p>
+      <p className="text-yellow-600 text-2xl mb-2">---- {subHeading} ----</p>
       <h3 className="text-4xl uppercase border-y-4 py-4 text-white">
         {heading}
       </h3>
