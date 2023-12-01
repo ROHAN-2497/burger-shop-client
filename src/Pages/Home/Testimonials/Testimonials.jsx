d965662fca1ebfee1fe33c7a085145496fbf5611
rohan future devoloper <xrohaislam373@gmail.com>
2023-12-01 19:17:19 +0600

    updated

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -32,7 +32,7 @@ const Testimonials = () => {
         {reviews.map((review) => (
           <SwiperSlide key={review._id}>
             <div className=" m-24 ">
-                
+
               <p>{review.details}</p>
               <h3 className="text-2xl text-orange-400">{review.name}</h3>
             </div>
