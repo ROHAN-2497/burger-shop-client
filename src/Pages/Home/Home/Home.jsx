ae28c065db3a9a5eebf89fda0f1eac7ffa6ad8db
rohan future devoloper <xrohaislam373@gmail.com>
2023-12-01 06:24:57 +0600

    bug fixed

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,13 +1,12 @@
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
-import PopularMenu from "./PopularMenu/PopularMenu";
 
 const Home = () => {
     return (
         <div>
            <Banner/>
            <Category/>
-           <PopularMenu/>
+          
         </div>
     );
 };
