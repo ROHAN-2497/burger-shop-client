5ac83d3b34f327617dd7c083c88af62b862c6be9
rohan future devoloper <xrohaislam373@gmail.com>
2023-12-04 02:31:15 +0600

    meni bannerr section

--- src/Pages/Menu/Cover/Cover.jsx
+++ src/Pages/Menu/Cover/Cover.jsx
@@ -1,10 +1,27 @@
-
-const Cover = () => {
-    return (
-        <div>
-            
+import Tilt from "react-parallax-tilt";
+const Cover = ({ img, title }) => {
+  return (
+    <div>
+      <Tilt>
+        <div style={{ height: "700px", backgroundColor: "darkgreen" }}>
+          <div
+            className="hero h-[700px]"
+            style={{
+              backgroundImage: `url("${img}")`,
+            }}
+          >
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+              <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+                <p className="mb-5">Would you like to try a dish?</p>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </Tilt>
+    </div>
+  );
 };
 
 export default Cover;
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,13 @@
 import { Helmet } from "react-helmet";
 import Cover from "../Cover/Cover";
-
+import menuImg from "../../../assets/menu/banner3.jpg";
 const Menu = () => {
   return (
     <div>
       <Helmet>
-      <title>Burger Shop | Menu </title>
+        <title>Burger Shop | Menu </title>
       </Helmet>
-      <Cover></Cover>
+      <Cover img={menuImg} title="Our Menu"></Cover>
     </div>
   );
 };
