94bc6fd972e03fc33a679a8f9e3088e65787702c
rohan future devoloper <xrohaislam373@gmail.com>
2023-12-05 02:24:50 +0600

    foodCard Section

---
+++ src/Component/FoodCard/FoodCard.jsx
@@ -0,0 +1,23 @@
+const FoodCard = ({ item }) => {
+  const { name, recipe, image, price } = item;
+
+  return (
+    <div className="card card-compact w-full">
+      <figure>
+        <img
+          src={image}
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>{recipe}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodCard;
--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -3,10 +3,18 @@ import banner from "../../../assets/shop/order-banner2.jpg";
 import Cover from "../../Menu/Cover/Cover";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import useMenu from "../../../Hooks/useMenu";
+import FoodCard from "../../../Component/FoodCard/FoodCard";
 const Order = () => {
   const [tabIndex, setTabIndex] = useState(0);
+  const [menu] = useMenu();
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const pizzas = menu.filter((item) => item.category === "pizza");
+  const salads = menu.filter((item) => item.category === "salad");
+  const soups = menu.filter((item) => item.category === "soup");
+  const offered = menu.filter((item) => item.category === "offered");
   return (
-    <div>
+    <div > 
       <Cover img={banner} title={"Order Food"}></Cover>
       <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
         <TabList>
@@ -16,7 +24,16 @@ const Order = () => {
           <Tab>dessert</Tab>
           <Tab>drink</Tab>
         </TabList>
-        <TabPanel></TabPanel>
+        <TabPanel>
+            <div className="grid md:grid-cols-3 gap-10  ">
+                {
+                    salads.map(item => <FoodCard
+                    key={item._id}
+                    item={item}
+                    ></FoodCard>)
+                }
+            </div>
+        </TabPanel>
         <TabPanel></TabPanel>
         <TabPanel></TabPanel>
         <TabPanel></TabPanel>
