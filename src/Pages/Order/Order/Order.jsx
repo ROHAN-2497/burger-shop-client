122e804e9e95fa78eed4798f4a9eff44307c350c
rohan future devoloper <xrohaislam373@gmail.com>
2023-12-05 02:07:56 +0600

    updated

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,23 +1,29 @@
-import { useState } from 'react';
-import banner from '../../../assets/shop/order-banner2.jpg'
-import Cover from '../../Menu/Cover/Cover';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import { useState } from "react";
+import banner from "../../../assets/shop/order-banner2.jpg";
+import Cover from "../../Menu/Cover/Cover";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 const Order = () => {
-    const [tabIndex, setTabIndex] = useState(0);
-    return(
-        <div>
-            <Cover img={banner} title={"Order Food"}></Cover>
-            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-  <TabList>
-    <Tab>Title 1</Tab>
-    <Tab>Title 2</Tab>
-  </TabList>
-  <TabPanel></TabPanel>
-  <TabPanel></TabPanel>
-</Tabs>
-        </div>
-    );
+  const [tabIndex, setTabIndex] = useState(0);
+  return (
+    <div>
+      <Cover img={banner} title={"Order Food"}></Cover>
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>salad</Tab>
+          <Tab>pizza</Tab>
+          <Tab>soup</Tab>
+          <Tab>dessert</Tab>
+          <Tab>drink</Tab>
+        </TabList>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Order;
