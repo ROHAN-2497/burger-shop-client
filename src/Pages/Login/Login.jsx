988e0e782ed5e71feb15f65ea69761067cbdc878
rohan future devoloper <xrohaislam373@gmail.com>
2023-12-08 01:29:29 +0600

    react recaptcha

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,54 +1,62 @@
-import logpic from '../../assets/others/authentication1.png'
-import Tilt from 'react-parallax-tilt';
+import logpic from "../../assets/others/authentication1.png";
+import Tilt from "react-parallax-tilt";
 
 const Login = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
   return (
-    <div className="hero min-h-screen">
-      <div className="hero-content flex-col lg:flex-row  gap-20">
+    <div>
+      <div className="hero min-h-screen ">
+        <div className="hero-content flex-col  px-20 lg:flex-row">
+          <Tilt className="">
+            <div className="w-3/4 ">
+              <img src={logpic} alt="" />
+            </div>
+          </Tilt>
 
-      <Tilt>
-      <div style={{  backgroundColor: 'darkgreen' }}>
-      <div className="text-center lg:text-left w-1/2">
-         <img src={logpic} alt="" />
-        </div>
-      </div>
-    </Tilt>
-  
-        
-        <div className="card w-1/2 shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <h1 className="text-5xl font-bold text-white">Login now!</h1>
-          <form className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="email"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
-            </div>
-          </form>
+          <div className="card  md:w-full  shadow-2xl bg-base-100">
+            <form onSubmit={handleLogin} className="card-body">
+              <h1 className="text-5xl font-bold  flex justify-center items-center text-black">
+                Login now!
+              </h1>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Login"
+                />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
