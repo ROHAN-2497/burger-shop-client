032da8fae07e4afc2864f818f2a4344f5969d475
rohan future devoloper <xrohaislam373@gmail.com>
2023-12-14 02:14:10 +0600

    updated

--- src/Component/FoodCard/FoodCard.jsx
+++ src/Component/FoodCard/FoodCard.jsx
@@ -1,17 +1,30 @@
 import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
 import Swal from "sweetalert2";
-import { useLocation, useNavigate } from "react-router-dom";
+import {  useLocation, useNavigate } from "react-router-dom";
 
 const FoodCard = ({ item }) => {
-  const { name, recipe, image, price } = item;
+  const { name, recipe, image, price, _id } = item;
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
   const handleAddtoCart = (item) => {
     console.log(item);
-    if (user) {
-      fetch("http://localhost:5000/carts")
+    if (user && user.email) {
+      const cartItem = {
+        menuItemId: _id,
+        name,
+        price,
+        image,
+        email: user.email,
+      };
+      fetch("http://localhost:5000/carts", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(cartItem),
+      })
         .then((res) => res.json())
         .then((data) => {
           if (data.insertedId) {
