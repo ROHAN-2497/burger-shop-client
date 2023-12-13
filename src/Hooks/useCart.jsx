7f3301760ff796e7cb1ffe019883fed68005165b
rohan future devoloper <xrohaislam373@gmail.com>
2023-12-14 03:45:00 +0600

    updated

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -1,4 +1,17 @@
-const useCart = email =>{
-
-}
+import { useContext } from "react";
+import { useQuery } from "react-query";
+import { AuthContext } from "../Providers/AuthProvider";
+const useCart = () => {
+  const { user } = useContext(AuthContext);
+  const { isLoading, refetch, data: cart = [] } = useQuery({
+    queryKey: ["carts", user?.email],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/carts?email=${user.email}`
+      );
+      return res.json();
+    },
+  });
+  return [cart, refetch, isLoading];
+};
 export default useCart;
