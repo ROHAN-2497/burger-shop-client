7a45104c717f31e2ad4bd515bb9f00eeedb40ab1
rohan future devoloper <xrohaislam373@gmail.com>
2023-12-21 11:06:12 +0600

    up

--- src/Pages/DashBoard/AllUsers/AllUsers.jsx
+++ src/Pages/DashBoard/AllUsers/AllUsers.jsx
@@ -1,14 +1,31 @@
 import { Helmet } from "react-helmet";
 import { FaTrash, FaUserShield } from "react-icons/fa";
 import { useQuery } from "react-query";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const { data: users = [], refetch } = useQuery(["users"], async () => {
     const res = await fetch("http://localhost:5000/users");
     return res.json();
   });
-
-  const handleDelete = () => {};
+  const handleMakeAdmin = (user) => {
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is Admin Now`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+  const handleDelete = (user) => {};
   return (
     <div className="bg-white text-black p-8">
       <Helmet>
@@ -38,7 +55,7 @@ const AllUsers = () => {
                     "admin"
                   ) : (
                     <button
-                      onClick={() => handleMakeAdmin(user._id)}
+                      onClick={() => handleMakeAdmin(user)}
                       className="btn btn-ghost btn-sm h-10 bg-slate-500 text-white"
                     >
                       <FaUserShield />
